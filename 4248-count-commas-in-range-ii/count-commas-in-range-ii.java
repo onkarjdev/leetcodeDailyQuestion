284878dfd6794a4e84506bf27fa89c48eb2d967a
Onkar S Sagale <onkarsagale2965@gmail.com>
2026-09-09 19:52:20 +0530

    Time: 1 ms (99.36%) | Memory: 42.9 MB (24.36%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long countCommas(long n) {
+        long totalCommas = 0;
+        long threshold = 1000; 
+        while (n >= threshold) {
+            totalCommas += (n - threshold + 1);
+            if (threshold > Long.MAX_VALUE / 1000) {
+                break;
+            }
+            threshold *= 1000;
+        }
+
+        return totalCommas;
+    }
+}
